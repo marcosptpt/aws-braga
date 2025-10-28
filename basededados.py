a7c273aad136485a93390362063f73256fce44f3
marcosptpt <mf95pt@gmail.com>
2025-10-28 11:37:25 +0000

    Create basededados.py

---
+++ basededados.py
@@ -0,0 +1,12 @@
+# Dados para conectar à BD
+
+import sys
+
+servidor = sys.argv[1]
+user = sys.argv[2]
+pass = "**********"
+
+
+print(f"O servidor é: {servidor}")
+print(f"O user é: {user}")
+print(f"A pass é: {pass}")
